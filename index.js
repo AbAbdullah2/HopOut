78d43c4d222d3135db0ecf46a785814f62ff2823
JohnDcruz <johndcruz@johndcruz.com>
2023-02-20 01:10:10 -0500

    fixed delete bug

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 });
 
 app.use(bodyParser.json());
