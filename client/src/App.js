540a59b6a705767b54550f983fbba0091e352366
emi ochoa <eoqwerty@gmail.com>
2023-02-20 23:03:13 -0500

    adding login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Hello from './components/Hello';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <div className="mx-auto h-screen flex flex-col items-center text-left justify-center relative">
-      <Hello />
-    </div>
+    <Router>
+      <div className="mx-auto h-screen flex flex-col items-center text-left justify-center relative">
+        <Routes>
+        <Route path="/" element={<Hello />}/>
+        <Route path="/login" element={<Login />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
