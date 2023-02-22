2296f3d849b81c7ed7cbb48a8ca6b4acd7a1f03a
JohnDcruz <johndcruz@johndcruz.com>
2023-02-22 14:43:32 -0500

    basic event styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import Hello from './components/Hello';
+import EventList from './components/EventList';
 
 const App = () => {
   return (
-    <div className="mx-auto h-screen flex flex-col items-center text-left justify-center relative">
-      <Hello />
+    <div>
+      <EventList />
     </div>
   );
 };
---
+++ client/src/components/EventList.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+  state = {
+    events: [
+      {
+        _id: 1,
+        title: 'HopOut Launch',
+        start: '2023-02-22T20:00:00Z',
+        end: '2023-02-22T22:00:00Z',
+        description: 'Launching HopOut',
+        image: 'https://picsum.photos/200/200',
+        location: '123 Main St, New York, NY 10001',
+      },
+      {
+        _id: 2,
+        title: 'HopOut IPO',
+        start: '2023-03-22T20:00:00Z',
+        end: '2023-03-22T22:00:00Z',
+        description: 'HopOuts IPO',
+        image: 'https://picsum.photos/200/200',
+        location: '123 Main St, New York, NY 10001',
+      },
+      {
+        _id: 3,
+        title: 'HopOut Sale',
+        start: '2023-04-22T20:00:00Z',
+        end: '2023-04-22T22:00:00Z',
+        description: 'Selling HopOut :(',
+        image: 'https://picsum.photos/200/200',
+        location: '123 Main St, New York, NY 10001',
+      },
+    ],
+  };
+
+  render() {
+    let { events } = this.state;
+
+    return (
+      <div className='mx-auto m-10 flex flex-col items-center justify-center h-full'>
+        <p className='text-4xl'>Event List</p>
+        <div className='mt-5 w-4/5'>
+          {
+            events.map((event) => {
+              const start = new Date(event.start);
+              const end = new Date(event.end);
+              return (
+                <div className='my-2 bg-slate-400 flex' key={event._id}>
+                  <img className='m-2 w-3/12' src={event.image} alt={event.title} />
+                  <div className='w-9/12 items-center justify-center text-center m-2 space-y-2'>
+                    <p className='text-4xl'>{event.title}</p>
+                    <p>{start.toDateString() + ' ' + start.getHours() + ':' + start.getMinutes() + ' - ' + end.getHours() + ':' + end.getMinutes()}</p>
+                    <p>{event.description}</p>
+                    <p>{event.location}</p>
+                  </div>
+                </div>
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EventList;
