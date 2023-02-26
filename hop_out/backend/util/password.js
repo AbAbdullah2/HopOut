2903e058ceeb4db6e5ceb88a57d6dd110082da81
Hope Menard <hope8654@gmail.com>
2023-02-26 12:25:19 -0500

    restructured to support ES6 for vitest

---
+++ hop_out/backend/util/password.js
@@ -0,0 +1,15 @@
+import bcrypt from "bcryptjs";
+
+export const hashPassword = (password) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    return hashedPassword;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const verifyPassword = (plainPassword, hashedPassword) => {
+  return bcrypt.compareSync(plainPassword, hashedPassword);
+};
---
+++ hop_out/test/backend/models/User.test.js
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import User from "../../../backend/models/User.js";
+import {faker} from "@faker-js/faker";
+import * as db from "../../../backend/index.js";
+import * as dotenv from "dotenv";
+
+test("test constructor", async () => {
+  const name = faker.name.fullName();
+  const email = faker.internet.email();
+  const password = faker.internet.password(8);
+  const user = await User.create({ name, email, password })
+  expect(user.name).toBe(name);
+  expect(user.email).toBe(email);
+  expect(user.id).toBeDefined();
+  expect(user.password).toBe(password);
+});
+
+test("two plus two is four", () => {
+    expect(2 + 2).toBe(4);
+  });
