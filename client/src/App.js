8a722482f7c19596b63db7ce7b1782965f978a48
JohnDcruz <johndcruz@johndcruz.com>
2023-03-01 16:28:18 -0500

    added detail page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
 import NotFound from './pages/NotFound';
+import EventDetail from './pages/EventDetail';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/">
           <Route index element={<Landing />} />
           <Route path="events" element={<EventList />} />
+          <Route path="events/:eventid" element={<EventDetail />} />
           <Route path="create" element={<Landing />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<CreateAccount />} />
--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,37 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { Card } from 'flowbite-react'
-
-function formatTime(date) {
-  const hour = date.getHours() % 12;
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  return (hour === 0 ? 12 : hour) + (minutes !== '00' ? ':' + minutes + ' ': '');
-}
-
-function AMPM(date) {
-  return date.getHours() >= 12 ? 'PM' : 'AM';
-}
-
-function formatDate(date) {
-  return (date.getMonth() + 1) + '/' + date.getDate();
-}
-
-function formatEventDates(start, end) {
-  const startString = formatDate(start) + ' ' + formatTime(start) + (AMPM(start) === AMPM(end) ? '' : AMPM(start));
-  const endString = (formatDate(end) === formatDate(start) ? '-' : ' - ' + formatDate(end) + ' ') + formatTime(end) + ' ' + AMPM(end);
-  return startString + endString;
-}
+import { formatEventDates } from '../helpers/FormatDate';
 
 export default function EventCard({ event }) {
   const start = new Date(event.event.start);
   const end = new Date(event.event.end);
   return (
-    <Card imgSrc={event.event.image} className='m-3 overflow-hidden'>
-      <div className='items-center justify-center text-center'>
-        <p className='text-2xl'>{event.event.title}</p>
-        <p className='my-2'><FontAwesomeIcon icon={solid('calendar')} /> {formatEventDates(start, end)}</p>
-        <p><FontAwesomeIcon icon={solid('location-dot')} /> {event.event.city}, {event.event.state} {event.event.zip}</p>
-      </div>
-    </Card>
+    <a href={'/events/' + event.event._id}>
+      <Card imgSrc={event.event.thumbnail} className='m-3 overflow-hidden hover:bg-blue-400'>
+        <div className='items-center justify-center text-center'>
+          <p className='text-2xl'>{event.event.title}</p>
+          <p className='my-2'><FontAwesomeIcon icon={solid('calendar')} /> {formatEventDates(start, end)}</p>
+          <p><FontAwesomeIcon icon={solid('location-dot')} /> {event.event.city}, {event.event.state} {event.event.zip}</p>
+        </div>
+      </Card>
+    </a>
   );
 }
