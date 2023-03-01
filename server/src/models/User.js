2d081d1ffd2b1e214ae24ca1dc211629c36889ae
Rohan Abichandani <rohanabichandani@gmail.com>
2023-03-01 17:47:20 -0500

    commented email in schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -33,14 +33,14 @@ const UserSchema = new mongoose.Schema({
   }]
 })
 
-UserSchema.path('email').validate((input) => {
-  try {
-    z.string().email().parse(input);
-    return true;
-  } catch (err) {
-    return false;
-  }
-}, 'Invalid Email');
+// UserSchema.path('email').validate((input) => {
+//   try {
+//     z.string().email().parse(input);
+//     return true;
+//   } catch (err) {
+//     return false;
+//   }
+// }, 'Invalid Email');
 
 
 const User = mongoose.model("User", UserSchema);
