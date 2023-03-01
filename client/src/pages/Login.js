ec3bd09bf14918f5d98b8558002b92c3d3a063eb
emi ochoa <eoqwerty@gmail.com>
2023-03-01 11:34:26 -0500

    adding styling to create account and login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -86,18 +86,18 @@ function Login(props) {
                 <div>
                 <button
                     type="submit"
-                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-400 py-2 px-4 text-sm font-medium text-white hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     onClick={(e) => login(e)}
                 >
                     <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <FontAwesomeIcon icon={solid('lock')} className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                    <FontAwesomeIcon icon={solid('lock')} className="h-5 w-5 text-blue-500 group-hover:text-slate-600" aria-hidden="true" />
                     </span>
                     Sign in
                 </button>
                 </div>
             </form>
             <button
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-400 py-2 px-4 text-sm font-medium text-white hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                 onClick={(e) => navigate('/createaccount')}
             >
                 Create a new account 
