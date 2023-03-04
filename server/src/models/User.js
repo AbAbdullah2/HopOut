d1fe8ea20ea4a4da68c1ed1a0814498a518e2d88
Rohan Abichandani <rohanabichandani@gmail.com>
2023-03-04 17:56:43 -0500

    deleted comment

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -33,16 +33,6 @@ const UserSchema = new mongoose.Schema({
   }]
 })
 
-// UserSchema.path('email').validate((input) => {
-//   try {
-//     z.string().email().parse(input);
-//     return true;
-//   } catch (err) {
-//     return false;
-//   }
-// }, 'Invalid Email');
-
-
 const User = mongoose.model("User", UserSchema);
 
 export default User;
