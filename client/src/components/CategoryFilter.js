d6be899f593556f7b08ab0ae42c3056bf1095da9
Rohan Abichandani <rohanabichandani@gmail.com>
2023-03-13 17:32:37 -0400

    filter visible events by category

--- client/src/components/CategoryFilter.js
+++ client/src/components/CategoryFilter.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Dropdown } from 'flowbite-react';
 
 export default function CategoryFilter(props) {
   const { selectedFilters, setSelectedFilters } = props;
@@ -10,31 +11,26 @@ export default function CategoryFilter(props) {
   ];
 
   const setChecked = (v) => {
-    let sf = selectedFilters;
-    if (sf.includes(v)) {
-      sf = sf.filter((f) => {return f !== v});
+    if (selectedFilters.includes(v)) {
+      setSelectedFilters(selectedFilters.filter((f) => {return f !== v}));
     } else {
-      sf.push(v);
+      setSelectedFilters([...selectedFilters, v]);
     }
-    console.log(sf);
-    setSelectedFilters(sf);
   }
 
   return (
-    <div>
-      <button id="dropdownCheckboxButton" data-dropdown-toggle="dropdownDefaultCheckbox" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown checkbox <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
-      <div id="dropdownDefaultCheckbox" className="z-10 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-          <ul className="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownCheckboxButton">
-            {filters.map((f) => (
-              <li key={f.key}>
-                <div className="flex items-center">
-                  <input id="checkbox-item-1" type="checkbox" checked={selectedFilters.includes(f.value)} onChange={(e) => {setChecked(f.value)}} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                  <label htmlFor="checkbox-item-1" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{f.value}</label>
-                </div>
-              </li>
-            ))}
-          </ul>
-      </div>
+    <div className="mt-2">
+      <Dropdown
+        label={"Filters"}
+        dismissOnClick={false}
+      >
+      {filters.map((f) => (
+        <Dropdown.Item key={f.key}>
+          <input id="checkbox-item-1" type="checkbox" checked={selectedFilters.includes(f.value)} onChange={(e) => {setChecked(f.value)}} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 p-2" />
+          <span className="pl-2">{f.value}</span>
+        </Dropdown.Item>
+      ))}
+    </Dropdown>
     </div>
   );
 }
--- client/src/pages/EventList.js
+++ client/src/pages/EventList.js
@@ -18,6 +18,20 @@ export function EventList(props) {
     });
   }, []);
 
+  const toDisplayEvent = (ev) => {
+    if (selectedFilters.length === 0) {
+      return true;
+    }
+    for (const f in selectedFilters) {
+      for (const cat in ev.categories) {
+        if (f === cat) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
   return (
     <div className='bg-stone-100 min-h-screen'>
       <div className='mx-auto flex flex-col items-center justify-center h-full'>
@@ -26,7 +40,10 @@ export function EventList(props) {
           <CategoryFilter selectedFilters={selectedFilters} setSelectedFilters={setSelectedFilters} />
         </div>
         <div className='my-5 w-11/12 md:grid md:grid-cols-3 items-center justify-center'>
-          {eventList.map((event) => {
+          {eventList.filter((ev) => {
+            return toDisplayEvent(ev);
+          })
+          .map((event) => {
             return (
               <EventCard key={event._id} event={event}/>
             );
