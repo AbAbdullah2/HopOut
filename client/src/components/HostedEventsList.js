057c55945cde317306e18b842b23fedffa61c32a
emi ochoa <eoqwerty@gmail.com>
2023-04-02 14:20:44 -0400

    fixing spacing hostedeventslist

--- client/src/components/HostedEventsList.js
+++ client/src/components/HostedEventsList.js
@@ -22,7 +22,6 @@ export default function MyEventsList(props) {
     <div className="text-xl w-full px-4 py-2 border-b-2 font-semibold text-slate-900">Hosted Events</div>
 
     <div className='my-5 w-11/12 md:grid md:grid-cols-2 items-center justify-center'>
-
         { 
         events.length > 0 ? 
         events.map((event) => 
@@ -30,15 +29,10 @@ export default function MyEventsList(props) {
                 <EventCard event={event} map={false} />
             </div>
         ) :
-        <div className="flex-1 min-w-0">
-
-            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+        <div className="flex-1 p-5">
+            <p className="text-sm font-medium text-gray-900 dark:text-white">
                 No events yet! 
             </p>
-            <p className="text-sm font-medium text-gray-500 truncate dark:text-white">
-                Search for users or see event invitees to add events. 
-            </p>
-
         </div>
         }
         
