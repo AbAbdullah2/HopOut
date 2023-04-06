823edd8c3d978ab2082917c264a243864842fd0d
jasleen-k <jasleenkaur0326@gmail.com>
2023-04-06 16:53:39 -0400

    started adding socket.io to backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
-import app from "./src/index.js";
-import * as db from "./src/data/db.js";
+import app from './src/index.js';
+import * as db from './src/data/db.js';
+import { Server } from 'socket.io';
 
 //db.connect(process.env.TEST_DB)
 db.connect(process.env.DB);
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, () => {
-    console.log(`HopOut API at http://localhost:${PORT}/`);
+const server = app.listen(PORT, () => {
+  console.log(`HopOut API at http://localhost:${PORT}/`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:${PORT}/`,
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on('connection', (socket) => {
+  global.chatSocket = socket;
+  socket.on('add-user', (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on('send-msg', (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit('msg-recieved', data.message);
+    }
+  });
 });
